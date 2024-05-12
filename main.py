9925612bf8e08b6ec363fa37465aa4161f0aaf72
balintt9828 <toth.balint@agilexpert.hu>
2024-05-12 12:10:17 +0200

    Generalise data variable name

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import pandas as pd
 from DecisionTree import Node
 
-wine_data = pd.read_csv('./datasets/winequality-red.csv')
+data = pd.read_csv('./datasets/winequality-red.csv')
 
 model = Node(method="MSE")
-train_split, test_split = model.split_data(wine_data, random_state=42, ratio=0.01)
+train_split, test_split = model.split_data(data, random_state=42, ratio=0.01)
 
 X_train = train_split.iloc[:, :-1]
 Y_train = train_split[['quality']]
